588bf427b0e0bf68fdc512ec5de8ce48c540c644
omkar1gen <omkar@1gen.io>
2024-01-30 15:30:00 +0530

    changes in q68_WordSearch

--- Array/q68_WordSearch.java
+++ Array/q68_WordSearch.java
@@ -1,3 +1,22 @@
+
+//Word Search
+
+/*
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+Example 1:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+Example 2:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+
+Example 3:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false*/
+
 package Array;
 
 public class q68_WordSearch {
@@ -5,7 +24,7 @@ public class q68_WordSearch {
     public boolean exist(char[][] board, String word) {
         int rows = board.length;
         int cols = board[0].length;
-
+        
         // Iterate through the board to find the starting letter of the word
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
