c9e49afacdab51e44f64d00b631296a5f5924ddc
Omkar Jadhav <93418994+Omkar9911@users.noreply.github.com>
2025-01-13 21:58:09 +0530

    update q46_DuplicateCharacters

--- String/q46_DuplicateCharacters.java
+++ String/q46_DuplicateCharacters.java
@@ -1,3 +1,20 @@
+
+/*
+Given a string S, the task is to print all the duplicate characters with their occurrences in the given string.
+
+        Example:
+
+Input: S = “geeksforgeeks”
+Output:
+e, count = 4
+g, count = 2
+k, count = 2
+s, count = 2
+
+Explanation: g occurs 2 times in the string, k occurs 2 times in the string, s occurs 2 times in the string while e occurs 4 times in the string rest of the characters have unique occurrences.
+*/
+
+
 package String;
 
 public class q46_DuplicateCharacters {
