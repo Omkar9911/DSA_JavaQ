c050344a2861f2a7bea4707789cae64a533af3f5
Omkar Jadhav <93418994+Omkar9911@users.noreply.github.com>
2025-01-24 20:59:57 +0530

    changes in question Q78_Find_common_arrays_Elements

--- String/Q78_Find_common_arrays_Elements.java
+++ String/Q78_Find_common_arrays_Elements.java
@@ -1,3 +1,18 @@
+
+//Find common elements in three sorted arrays
+
+/*
+Given three sorted arrays in non-decreasing order, print all common elements in these arrays.
+Note: In case of duplicate common elements, print only once.
+
+Examples:
+Input: A[] = {1, 5, 10, 20, 30} , B[] = {5, 13, 15, 20}, C[] = {5, 20}
+Output: 5 20
+Explanation: 5 and 20 are common in all the arrays.
+
+Input: A[] = {2, 5, 10, 30}, B[] = {5, 20, 34}, C[] = {5, 13, 19}
+Output: 5
+*/
 package String;
 
 import java.util.ArrayList;
